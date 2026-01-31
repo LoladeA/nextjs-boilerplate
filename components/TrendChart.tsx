bbbc988411f36f76004030a1a4e2a5a57d013190
Lolade <lolade_ajai@hotmail.com>
2026-01-31 15:50:01 +0100

    Create TrendChart.tsx

---
+++ components/TrendChart.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { FC } from "react";
+
+const TrendChart: FC = () => {
+  return (
+    <div className="w-full h-64 bg-gray-100 flex items-center justify-center rounded-md">
+      <p className="text-gray-500">Trend Chart Placeholder</p>
+    </div>
+  );
+};
+
+export default TrendChart;
