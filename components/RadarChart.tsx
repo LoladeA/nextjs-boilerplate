32cfc79384ce52d60a19bd90a8e78fa7a9e12a7d
Lolade <lolade_ajai@hotmail.com>
2026-01-31 15:48:54 +0100

    Create RadarChart.tsx

---
+++ components/RadarChart.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { FC } from "react";
+
+const RadarChart: FC = () => {
+  return (
+    <div className="w-full h-64 bg-gray-100 flex items-center justify-center rounded-md">
+      <p className="text-gray-500">Radar Chart Placeholder</p>
+    </div>
+  );
+};
+
+export default RadarChart;
