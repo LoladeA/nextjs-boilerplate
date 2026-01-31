5e264a2aad213c977eb5a036e059dfbf75f8bf39
Lolade <lolade_ajai@hotmail.com>
2026-01-31 10:36:01 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,7 @@ export default function Footer() {
   return (
     <footer className="px-6 py-12 text-center text-sm">
       <p>
-        © {(Febuary).(2026)} The Sentient Home.
-        Residential Spaces Designed for Clarity, Agency, and Nervous System Safety.
+        February 2026 — The Sentient Home: Residential Spaces Designed for Clarity, Agency, and Nervous System Safety.
       </p>
     </footer>
   )
