73ea690411e65b43430ffceda4aa68e59a3cf363
Lolade <lolade_ajai@hotmail.com>
2026-01-31 12:01:08 +0100

    Update useSupabaseClient.ts

--- lib/useSupabaseClient.ts
+++ lib/useSupabaseClient.ts
@@ -1,14 +1,11 @@
 'use client'
 
-import { createClient, SupabaseClient } from '@supabase/supabase-js'
 import { useMemo } from 'react'
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
 export function useSupabaseClient(): SupabaseClient {
   return useMemo(() => {
-    if (
-      !process.env.NEXT_PUBLIC_SUPABASE_URL ||
-      !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-    ) {
+    if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
       throw new Error('Supabase environment variables are missing')
     }
     return createClient(
