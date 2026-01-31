1ce652287a3bc09978eefffec7f67a558ee51f20
Lolade <lolade_ajai@hotmail.com>
2026-01-31 10:10:13 +0100

    Create HomePage component with multiple sections

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import HeroSection from '@/components/HeroSection'
+import WhatThisIs from '@/components/WhatThisIs'
+import HowItWorks from '@/components/HowItWorks'
+import WhatYouWillLearn from '@/components/WhatYouWillLearn'
+import HowToUse from '@/components/HowToUse'
+import AuthPlaceholder from '@/components/AuthPlaceholder'
+import Footer from '@/components/Footer'
+
+export default function HomePage() {
+  return (
+    <main className="flex flex-col">
+      <HeroSection />
+      <WhatThisIs />
+      <HowItWorks />
+      <WhatYouWillLearn />
+      <HowToUse />
+      <AuthPlaceholder />
+      <Footer />
+    </main>
+  )
+}
