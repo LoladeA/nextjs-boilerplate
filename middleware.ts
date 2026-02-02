102a99baced04ceeb85ceef3f49d2b409851f8c5
Lolade <lolade_ajai@hotmail.com>
2026-02-02 16:14:09 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -5,9 +5,6 @@ import type { NextRequest } from 'next/server'
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req, res })
-  
-  // This refreshes the session if it's expired
   await supabase.auth.getSession()
-  
   return res
 }
