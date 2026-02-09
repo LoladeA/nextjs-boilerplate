02a0b2f1e3ce8b103b858b2dcf4f453cc19e4714
Lolade <lolade_ajai@hotmail.com>
2026-02-09 12:11:27 +0100

    Update page.tsx

--- app/upgrade/page.tsx
+++ app/upgrade/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import { Sparkles, CheckCircle, ArrowRight, Brain, Shield, ExternalLink, Gem, Loader2 } from 'lucide-react'
